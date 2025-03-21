2c381e38c0f83e50578ac4462165c6519ab03e33
AEToluca <aeestrada1004@gmail.com>
2025-03-21 10:47:29 -0400

    toString

--- coin.java
+++ coin.java
@@ -4,10 +4,18 @@ public class coin {
     private String frontMotto;
     private int manufactureYear;
     private String frontImage;
+    private String backImage;
     private String backMotto;
+    private String frontLabel;
     private String backLabel;
     private String valueDescription;
     private boolean ridgedEdge;
     private String metallurgy;
-    
+
+    //This method prints the information of the coin.
+    public String toString() {
+        return "[" + commonName + "," + value + "," + manufactureYear + ",'" + frontMotto + "','" + backMotto + "','"
+                + frontImage + "','" + backImage + "','" + frontLabel + "','" + backLabel + "','" + valueDescription + "',"
+                + (ridgedEdge ? "ridges" : "no-ridges") + ",'" + metallurgy + "']";
+    }
 }
