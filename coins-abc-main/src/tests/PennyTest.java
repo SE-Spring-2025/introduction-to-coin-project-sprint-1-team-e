531fff68c01c792eb1c8959f823d4d91e5f10240
Mitchel Carson <mitchel.carson@gmail.com>
2025-03-27 12:43:31 -0400

    Added test files for all coins.

---
+++ coins-abc-main/src/tests/PennyTest.java
@@ -0,0 +1,10 @@
+import java.util.Calendar;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class PennyTest {
+    
+}
