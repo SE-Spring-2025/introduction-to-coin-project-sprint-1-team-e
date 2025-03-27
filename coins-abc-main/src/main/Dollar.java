e17f7c5afef19d43d06eaf408d84688b168c2bf3
Carson Taylor <taylorcj6@appstate.edu>
2025-03-27 13:41:09 -0400

    Added constructor to Dollar and Nickel

--- coins-abc-main/src/main/Dollar.java
+++ coins-abc-main/src/main/Dollar.java
@@ -1,8 +1,8 @@
 public class Dollar extends Coin {
     public Dollar(){
-
+        super("Dollar", 1.00, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "S_Anthony", "Moon_Eagle", "ONE DOLLAR", true, "Cupro-Nickel", 2025);
     }
     public Dollar(int year){
-
+        super("Dollar", 1.00, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "S_Anthony", "Moon_Eagle", "ONE DOLLAR", true, "Cupro-Nickel", year);
     }
 }
--- coins-abc-main/src/main/Nickel.java
+++ coins-abc-main/src/main/Nickel.java
@@ -1,8 +1,8 @@
 public class Nickel extends Coin{
    public Nickel(){
-
+    super("Nickel", .05, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "T_Jefferson", "Jefferson_Memorial", "FIVE CENTS", false, "Cupro-Nickel", 2025);
    }
    public Nickel(int year){
-
+    super("Nickel", .05, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "T_Jefferson", "Jefferson_Memorial", "FIVE CENTS", false, "Cupro-Nickel", year);
    }
 }
