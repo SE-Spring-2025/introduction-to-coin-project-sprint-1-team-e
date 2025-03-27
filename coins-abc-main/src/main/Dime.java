7692135d9e3fbbf8490fae3d59c08ddda887cc72
Carson Taylor <taylorcj6@appstate.edu>
2025-03-27 13:32:52 -0400

    added to coin.

--- coins-abc-main/src/main/Dime.java
+++ coins-abc-main/src/main/Dime.java
@@ -6,6 +6,6 @@ public class Dime extends Coin{
         super("Dime", 0.1, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "F_Roosevelt", "Torch_Branches", "ONE DIME", true, "Cupro-Nickel", 2025);
     }
     public Dime(int year){
-        
+        super("Dime", 0.1, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "F_Roosevelt", "Torch_Branches", "ONE DIME", true, "Cupro-Nickel", year);
     }
 }
