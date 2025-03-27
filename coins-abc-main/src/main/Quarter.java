b5ae33ec617190e9c8fdef40b688d27f3e8b675c
Jenna Mena <jmena8448@gmail.com>
2025-03-27 13:48:13 -0400

    Penny, Quarter, and HalfDollar classes now have the no-arg and one arg constructor completed.

--- coins-abc-main/src/main/Quarter.java
+++ coins-abc-main/src/main/Quarter.java
@@ -1,8 +1,10 @@
+import java.time.ZonedDateTime;
+
 public class Quarter extends Coin{
     public Quarter(){
-        
+        super("Quarter", 0.25, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "G_Washington", "Eagle", "QUARTER DOLLAR", true, "Cupro-Nickel", 2025);
     }
     public Quarter(int year){
-
+        super("Quarter", 0.25, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "G_Washington", "Eagle", "QUARTER DOLLAR", true, "Cupro-Nickel", year);
     }
 }
