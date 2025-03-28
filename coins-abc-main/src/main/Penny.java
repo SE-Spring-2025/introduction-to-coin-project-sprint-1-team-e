de761b2e9c5f300788cab1e7795d9d371ea8052e
Mitchel Carson <mitchel.carson@gmail.com>
2025-03-28 16:08:56 -0400

    Fixed all checkstyle errors.

--- coins-abc-main/src/main/Penny.java
+++ coins-abc-main/src/main/Penny.java
@@ -1,8 +1,25 @@
-public class Penny extends Coin{
-    public Penny(){
-        super("Penny", 0.01, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "A_Lincoln", "Lincoln_Memorial", "ONE CENT", false, "Copper", 2025);
+/**
+ * Penny coin class.
+ * @author Team E
+ * @version 1.0
+ */
+public class Penny extends Coin {
+    /**
+     * Default constructor for Penny.
+     */
+    public Penny() {
+        super("Penny", PENNY_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
+            "LIBERTY", "UNITED STATES OF AMERICA", "A_Lincoln", 
+            "Lincoln_Memorial", "ONE CENT", false, "Copper", DEFAULT_YEAR);
     }
-    public Penny(int year){
-        super("Penny", 0.01, "IN GOD WE TRUST", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "A_Lincoln", "Lincoln_Memorial", "ONE CENT", false, "Copper", year);
+    
+    /**
+     * Constructor for Penny with specific year.
+     * @param year the year of manufacture
+     */
+    public Penny(int year) {
+        super("Penny", PENNY_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
+            "LIBERTY", "UNITED STATES OF AMERICA", "A_Lincoln", 
+            "Lincoln_Memorial", "ONE CENT", false, "Copper", year);
     }
 }
