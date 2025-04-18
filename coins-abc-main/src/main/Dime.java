36b17c7bac6022e917b008fbafacc44093eb2595
Jenna Mena <jmena8448@gmail.com>
2025-04-18 00:18:22 -0400

    Fixed Dime class and added changes to the Dollar class

--- coins-abc-main/src/main/Dime.java
+++ coins-abc-main/src/main/Dime.java
@@ -8,77 +8,57 @@ public class Dime extends Coin {
      * Default constructor for Dime.
      */
     public Dime() {
-        super("Dime", DIME_VALUE,
-            new CuproNickel(), DEFAULT_YEAR);
+        super("Dime", DIME_VALUE, null, null, 
+        null, null, null, 
+        null, null, false, 
+        null, DEFAULT_YEAR);
     }
     
-    // /**
-    //  * Constructor for Dime with specific year.
-    //  * @param year the year of manufacture
-    //  */
-    // public Dime(int year) {
-    //     super("Dime", DIME_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
-    //         "LIBERTY", "UNITED STATES OF AMERICA", "F_Roosevelt", 
-    //         "Torch_Branches", "ONE DIME", true, 
-    //         new CuproNickel(), year);
-    // }
-    
-    // /**
-    //  * Constructor for Dime with specific year and metallurgy.
-    //  * @param year the year of manufacture
-    //  * @param smelter the metallurgy strategy
-    //  */
-    // public Dime(int year, Metallurgy smelter) {
-    //     super("Dime", DIME_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
-    //         "LIBERTY", "UNITED STATES OF AMERICA", "F_Roosevelt", 
-    //         "Torch_Branches", "ONE DIME", true, 
-    //         smelter, year);
-    // }
+    /**
+     * Constructor for Dime with specific year.
+     * @param year the year of manufacture
+     */
+    public Dime(int year) {
+        super("Dime", DIME_VALUE, null, null, 
+            null, null, null, 
+            null, null, false, 
+            null, year);
+    }
 
-    @Override
     protected Coin smeltStep(Coin c) {
         setSmelter(new CuproNickel());
         smelt();
 
-        return this;
+        return c;
     }
 
-    //Note to self: Is this method needed?
-    @Override
     protected Coin ImprintRidgedEdge(Coin c) {
-        this.setRidgedEdge(false);
+        c.setRidgedEdge(false);
 
-        return this;
+        return c;
     }
 
-    @Override
     protected Coin ImprintFrontImage(Coin c) {
-        this.setFrontImage("F_Roosevelt");
+        c.setFrontImage("F_Roosevelt");
 
-        return this;
+        return c;
     }
 
-    @Override
     protected Coin ImprintBackImage(Coin c) {
-        this.setBackImage("Torch_Branches");
+        c.setBackImage("Torch_Branches");
 
-        return this;
+        return c;
     }
 
-    @Override
     protected Coin ImprintFrontMotto(Coin c) {
-        this.setFrontMotto("In God We Trust");
-        this.setFrontLabel("Liberty");
+        c.setFrontMotto("IN GOD WE TRUST");
 
-        return this;
+        return c;
     }
 
-    @Override
     protected Coin ImprintBackMotto(Coin c) {
-        this.setBackMotto("E Pluribus Unum");
-        this.setBackLabel("UNITED STATES OF AMERICA");
-        this.setValueDescription("ONE DIME");
+        c.setBackMotto("E PLURIBUS UNUM");
 
-        return this;
+        return c;
     }
 }
--- coins-abc-main/src/main/Dollar.java
+++ coins-abc-main/src/main/Dollar.java
@@ -8,9 +8,9 @@ public class Dollar extends Coin {
      * Default constructor for Dollar.
      */
     public Dollar() {
-        super("Dollar", DOLLAR_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
-            "LIBERTY", "UNITED STATES OF AMERICA", "S_Anthony", 
-            "Moon_Eagle", "ONE DOLLAR", true, new CuproNickel(), DEFAULT_YEAR);
+        super("Dollar", DOLLAR_VALUE, null, null, 
+            null, null, null, 
+            null, null, false, null, DEFAULT_YEAR);
     }
     
     /**
@@ -18,56 +18,45 @@ public class Dollar extends Coin {
      * @param year the year of manufacture
      */
     public Dollar(int year) {
-        super("Dollar", DOLLAR_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
-            "LIBERTY", "UNITED STATES OF AMERICA", "S_Anthony", 
-            "Moon_Eagle", "ONE DOLLAR", true, new CuproNickel(), year);
-    }
-    
-    /**
-     * Constructor for Dollar with specific year and metallurgy.
-     * @param year the year of manufacture
-     * @param smelter the metallurgy strategy
-     */
-    public Dollar(int year, Metallurgy smelter) {
-        super("Dollar", DOLLAR_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
-            "LIBERTY", "UNITED STATES OF AMERICA", "S_Anthony", 
-            "Moon_Eagle", "ONE DOLLAR", true, smelter, year);
+        super("Dollar", DOLLAR_VALUE, null, null, 
+            null, null, null, 
+            null, null, false, new CuproNickel(), year);
     }
 
     protected Coin smeltStep(Coin c) {
-        c.setSmelter(new DimeMetallurgy); //Definetely changing this
-        c.smelt();
+        setSmelter(new CuproNickel());
+        smelt();
 
-        return ;
+        return c;
     }
 
-    protected Coin ImprintridgedEdge(Coin c) {
-        
+    protected Coin ImprintRidgedEdge(Coin c) {
+        c.setRidgedEdge(true);
 
-        return ;
+        return c;
     }
 
     protected Coin ImprintFrontImage(Coin c) {
-        
+        c.setFrontImage("S_Anthony");
 
-        return ;
+        return c;
     }
 
     protected Coin ImprintBackImage(Coin c) {
-        
+        c.setBackImage("Moon_Eagle");
 
-        return ;
+        return c;
     }
 
     protected Coin ImprintFrontMotto(Coin c) {
-        
+        c.setFrontMotto("In GOD WE TRUST");
 
-        return ;
+        return c;
     }
 
     protected Coin ImprintBackMotto(Coin c) {
-        
+        c.setBackMotto("E PLURIBUS UNUM");
 
-        return ;
+        return c;
     }
 }
